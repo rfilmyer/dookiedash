cea1f93db4406fe042dd4add415ebfd93ff31ddb
Roger Filmyer <roger.filmyer@gmail.com>
2017-05-14 14:48:29 -0700

    forgot to add all of the actual functionality

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import json
-
 from aiohttp import web
 
-from notifications import send_email
-
+from notifications import format_email, send_email
+from order_form import create_shitexpress_order
 
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
@@ -11,13 +9,17 @@ async def handle(request):
     return web.Response(text=text.format(name=name))
 
 async def email(request):
-    email_response = send_email()
+    order_details = create_shitexpress_order()
+    plain_text_message, html_message = format_email(order_id=order_details['order_id'],
+                                                    address=order_details['address'],
+                                                    bitcoin_amt=order_details['amount'],
+                                                    wallet_id=order_details['btc_address'])
+    email_response = send_email(plain_text_message, html_message)
     return web.json_response(email_response)
 
 app = web.Application()
 app.router.add_get('/', handle)
 app.router.add_get('/email', email)
 app.router.add_post('/email', email)
-app.router.add_get('/{name}', handle)
 
 web.run_app(app, host='localhost', port=8000)
--- notifications.py
+++ notifications.py
@@ -1,16 +1,32 @@
 import boto3
 
+from email_text import plain_text_message_raw, html_message_raw
+
 ses = boto3.client('ses')
 
-from_email = "roger.filmyer@gmail.com"
+from_email = "dookiedashdelivery@gmail.com"
 
-def send_email(recipient=from_email):
+def format_email(order_id="A1B2C3",
+                 address="1600 Pennsylvania Avenue NW\nWashington, DC 20500",
+                 bitcoin_amt="0.0095",
+                 wallet_id="a1b2c3d4e5f60123456789"):
+    return (plain_text_message_raw.format(order_id=order_id,
+                                          address=address,
+                                          bitcoin_amt=bitcoin_amt,
+                                          wallet_id=wallet_id),
+            html_message_raw.format(order_id=order_id,
+                                    address=address.replace('\n', '<br>'),
+                                    bitcoin_amt=bitcoin_amt,
+                                    wallet_id=wallet_id))
+
+def send_email(plain_text_message, html_message, recipient=from_email):
     dest = {"ToAddresses": [recipient]}
-    message = {"Subject": {"Data": "Test 🐎💩✉️ Email", "Charset": "utf-8"},
-               "Body": {"Text": {"Data": "This is a test email. 💩. That was an emoji.", "Charset": "utf-8"}}}
+    message = {"Subject": {"Data": "Hey! 🐎💩✉️ Your Order is on its Way!", "Charset": "utf-8"},
+               "Body": {"Text": {"Data": plain_text_message, "Charset": "UTF-8"},
+                        "Html": {"Charset": "UTF-8", "Data": html_message}}}
     response = ses.send_email(Source=from_email, Destination=dest, Message=message)
     return response
 
-
 if __name__ == '__main__':
-    send_email()
+    plain_text_message, html_message = format_email()
+    send_email(plain_text_message, html_message)
